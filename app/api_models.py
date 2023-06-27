c7fbebfa6a2e4d407894b39e5ac6a66ff78fdcc1
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-06-27 15:17:33 +0300

    Added company model with no ID field

--- app/api_models.py
+++ app/api_models.py
@@ -2,8 +2,17 @@ from flask_restx import fields
 
 from extensions import api
 
-company_model = api.model('Company', {
-    'id': fields.Integer(readonly=True),
+company_model_no_id = api.model('Company', {
+    'name': fields.String,
+    'country': fields.String,
+    'locality': fields.String,
+    'industry': fields.String,
+    'linkedin_url': fields.String,
+    'domain': fields.String
+})
+
+company_model = api.model('PatchCompany', {
+    'id': fields.Integer,
     'name': fields.String,
     'country': fields.String,
     'locality': fields.String,
