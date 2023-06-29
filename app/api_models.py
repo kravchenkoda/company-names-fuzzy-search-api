85a54b59a27582e6cffa1ef35edafca976280cec
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-06-29 17:58:21 +0300

    Added multi-search-related models, reused code from company_model_no_id to company model

--- app/api_models.py
+++ app/api_models.py
@@ -2,7 +2,7 @@ from flask_restx import fields
 
 from extensions import api
 
-company_model_no_id = api.model('Company', {
+company_model_no_id = api.model('CompanyNoId', {
     'name': fields.String,
     'country': fields.String,
     'locality': fields.String,
@@ -11,12 +11,15 @@ company_model_no_id = api.model('Company', {
     'domain': fields.String
 })
 
-company_model = api.model('PatchCompany', {
-    'id': fields.Integer,
-    'name': fields.String,
-    'country': fields.String,
-    'locality': fields.String,
-    'industry': fields.String,
-    'linkedin_url': fields.String,
-    'domain': fields.String
+company_model = api.clone('Company', company_model_no_id, {
+    'id': fields.Integer
+})
+
+multisearch_response_element = api.model('MultiSearchElement', {
+    'search_request': fields.Nested(company_model_no_id),
+    'search_results': fields.List(fields.Nested(company_model))
+})
+
+multisearch_response_model = api.model('MultiSearchResponseModel', {
+    'data': fields.List(fields.Nested(multisearch_response_element))
 })
